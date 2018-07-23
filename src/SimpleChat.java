b54e0e99462addf6917ceef707cc13cd4318ea8f
Mozgachev I.V <mozgachev.ivan@gmail.com>
2018-07-23 14:39:37 +0300

    add new MVP model

---
+++ src/SimpleChat.java
@@ -0,0 +1,5 @@
+public class SimpleChat {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/presenter/ClientController.java
@@ -0,0 +1,7 @@
+package presenter;
+
+public class ClientController {
+    public static void main(String[] args) {
+
+    }
+}
