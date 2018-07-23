08791df772c55f112c96fbba35112fb23ddfd5f3
Mozgachev I.V <mozgachev.ivan@gmail.com>
2018-07-23 18:12:32 +0300

    Add Form chat

--- src/SimpleChat.java
+++ src/SimpleChat.java
@@ -1,7 +1,9 @@
 import model.Client;
+import view.MainForm;
 
 public class SimpleChat {
     public static void main(String[] args) {
-        Client client = new Client("Vasya");
+        //Client client = new Client("Vasya");
+        MainForm mf = new MainForm();
     }
 }
--- src/view/MainForm.java
+++ src/view/MainForm.java
@@ -2,8 +2,32 @@ package view;
 
 import presenter.ClientPresenter;
 
-public class MainForm implements IView{
+import javax.swing.*;
+import java.awt.*;
+
+public class MainForm extends JFrame implements IView{
     private ClientPresenter clientPresenter;
+    private JTextArea messageField;
+    private JTextField message;
+
+
+    public MainForm() {
+        this.setSize(500, 600);
+        this.setTitle("Чат через ФС");
+        this.setLayout(new BorderLayout());
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+
+        messageField = new JTextArea();
+        message = new JTextField();
+        messageField.setEnabled(false);
+
+        this.add(messageField);
+        this.add(message, BorderLayout.SOUTH);
+
+        this.setVisible(true);
+        message.requestFocus();
+    }
 
     @Override
     public void setPresenter(ClientPresenter clientPresenter) {
